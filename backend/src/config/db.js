24e22727266336122b003c34b073d864b630c457
ritika1406 <68157805+ritika1406@users.noreply.github.com>
2026-09-21 15:18:25 +0530

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,10 +2,9 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-});
-
-pool.on("error", (err) => {
-  console.error("Unexpected PostgreSQL pool error:", err);
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
